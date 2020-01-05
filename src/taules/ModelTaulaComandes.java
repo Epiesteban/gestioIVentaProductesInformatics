af946a8fb57077fab7bc2ee10415cc57e52c0294
Xenia <xenia.fuentes@estudiants.urv.cat>
2020-01-05 13:13:02 +0100

    Xenia: lmao 2

--- src/taules/ModelTaulaComandes.java
+++
@@ -1,9 +0,0 @@
-package taules;
-
-public class ModelTaulaComandes {
-
-	public ModelTaulaComandes() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- src/taules/NomTaulaComandes.java
+++
@@ -1,9 +0,0 @@
-package taules;
-
-public class NomTaulaComandes {
-
-	public NomTaulaComandes() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
